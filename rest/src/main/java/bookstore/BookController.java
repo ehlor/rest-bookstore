10c7b25081aca895f644701d386c6aadc6baf2d8
theelo <38947148+theelo@users.noreply.github.com>
2019-03-13 14:33:29 +0200

    Update BookController.java

--- rest/src/main/java/bookstore/BookController.java
+++ rest/src/main/java/bookstore/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.constraints.Valid;
+import javax.validation.Valid;
 
 @RestController
 public class BookController{
